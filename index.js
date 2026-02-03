364021aa7effec459373d27c57126e0e5cfcb172
unknown <muktinathrajbanshi786@gmail.com>
2026-02-03 15:27:24 +0545

    now few songs remain to upload in this project

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ let song3 = new Audio();
 song3.src = "music/fly_like_that.mp3";
 
 let song4 = new Audio();
-song4.src = "music/back_to_your_side.mp3";
+song4.src = "music/lost_in_your_eyes.mp3";
 
 let song5 = new Audio();
-song5.src = "music/back_to_your_side.mp3";
+song5.src = "music/silk_and_fire.mp3";
 
 let song6 = new Audio();
-song6.src = "music/back_to_your_side.mp3";
+song6.src = "music/slowly_tonight.mp3";
 
 let song7 = new Audio();
-song7.src = "music/back_to_your_side.mp3";
+song7.src = "music/top_of_the_game.mp3";
 
 let song8 = new Audio();
-song8.src = "music/back_to_your_side.mp3";
+song8.src = "music/velvet_nights.mp3";
 
 let song9 = new Audio();
 song9.src = "music/back_to_your_side.mp3";
@@ -51,8 +51,32 @@ function playsong() {
         case "c":
             song3.play();
             break;    
+         
+        case "d":
+            song4.play();
+            break;   
             
-    
+        case "e":
+            song5.play();
+            break;     
+            
+        case "f":
+            song6.play();
+            break;    
+
+        case "g":
+            song7.play();
+            break;  
+
+        case "h":
+            song8.play();
+            break; 
+
+        case "i":
+            song9.play();
+            break;     
+
+
         default:
             console.log("wrong input");
             break;
